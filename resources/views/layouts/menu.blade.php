9ac8aaceb7ff8ed0d950df52f2602ce8fef862bf
baravak <itb.baravak@gmail.com>
2020-03-19 18:56:13 +0330

    translation

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -2,13 +2,13 @@
 @parent
     <li class="nav-item">
         <a class="nav-link text-truncate" href="{{route('dashboard.stories.index')}}">
-            <span class="d-sm-inline">{{__('Story')}}</span>
+            <span class="d-sm-inline">{{__('Stories')}}</span>
         </a>
     </li>
 
     <li class="nav-item">
         <a class="nav-link text-truncate" href="{{route('dashboard.posts.index')}}">
-            <span class="d-sm-inline">{{__('Post')}}</span>
+            <span class="d-sm-inline">{{__('Posts')}}</span>
         </a>
     </li>
 
