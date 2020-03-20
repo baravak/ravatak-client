bf458ce94ba3546d1446f14f8ff4915a570ed396
baravak <itb.baravak@gmail.com>
2020-03-20 12:41:39 +0330

    bugfix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,9 +10,6 @@ class HomeController extends Controller
 {
     public function index(Request $request)
     {
-        // $parser = new \cebe\markdown\Markdown();
-        // $x = $parser->parse('### Hi');
-        // dd($x);
         $this->data->special = Post::apiShow('P966666U');
         $this->data->stories = Story::apiIndex($request->all(['order', 'sort', 'status', 'page']));
         return $this->view($request, 'home');
@@ -20,6 +17,7 @@ class HomeController extends Controller
 
     public function show(Request $request, Story $story)
     {
+        dd($request->header());
         $this->data->global->title = 'زبان‌زد | ' . $story->story_number . ' - ' . $story->title;
         $this->data->story = $story;
         return $this->view($request, 'show');
