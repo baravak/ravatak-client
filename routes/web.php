cf99792ea3b5c4567a0007adae2a1e460441cfe2
baravak <itb.baravak@gmail.com>
2020-03-20 13:03:07 +0330

    bugfix

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/story/{story}', 'HomeController@show')->name('stories.show');
@@ -9,3 +10,6 @@ Route::get('+/{term}/term={subTerm}', 'TermPostController@index')->name('termPos
 Route::get('+/{term}/{post}', 'TermPostController@show')->name('termPost.show')->where(['post' => '/[^=]+/']);
 
 Route::get('/about-us', 'HomeController@aboutUs')->name('about.us');
+Route::get('/tst', function(Request $request){
+    dd($request->header());
+});
